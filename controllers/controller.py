d1c0b0ced64ff6b0923732f74f50c3ba5ba6b82e
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-01 15:30:46 +0000

    Completed Extension

--- controllers/controller.py
+++ controllers/controller.py
@@ -5,3 +5,8 @@ from models.order_list import orders
 @app.route('/orders')
 def index():
     return render_template('index.html', orders=orders)
+
+@app.route('/orders/<int:number>')
+def order(number):
+    number -= 1
+    return render_template('order.html', order_num = number + 1, order=orders[number])
