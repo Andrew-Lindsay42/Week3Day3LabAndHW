5ae4bde568f91f27d23ba92eab5cf6058a26a7d3
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-01 13:30:48 +0000

    Added ShoeOrder Class

---
+++ models/order.py
@@ -0,0 +1,8 @@
+class ShoeOrder():
+    def __init__(self, customer_name, order_date, quantity, shoe_brand, shoe_description, shoe_price):
+        self.customer_name = customer_name
+        self.order_date = order_date
+        self.quantity = quantity
+        self.shoe_brand = shoe_brand
+        self.shoe_description = shoe_description
+        self.shoe_price = shoe_price
