98f1205e1202c2789f65d738247ae3608493e497
Andrew Lindsay <andrew_e_lindsay@hotmail.com>
2021-12-01 14:34:50 +0000

    Flask will now run

--- models/order_list.py
+++ models/order_list.py
@@ -1,4 +1,4 @@
-from shoe_order import ShoeOrder
+from models.shoe_order import ShoeOrder
 
 order1 = ShoeOrder('Andrew', '01/12/2021', 1, 'Mizuno Waverider 25', 'A classic and versatile training shoe', 129.99)
 order2 = ShoeOrder('Jamie', '10/09/2021', 1, 'Nike ZoomX Vaporfly Next Percent 2', 'The ultimate go faster shoe for race day', 180.00)
